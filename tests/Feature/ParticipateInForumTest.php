209594357b5dc0370196424ee03e83b385dc3b89
dudulocao <edu_godoys@hotmail.com>
2018-11-06 22:38:10 -0200

    Aula 10 completa

--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -27,12 +27,25 @@ class ParticipateInForumTest extends TestCase
         $this->be($user = create('App\User'));
 
         $thread = create('App\Thread');
-
-        $reply = factory('App\Reply')->create(['thread_id' => $thread->id]);
+        $reply = make('App\Reply');
 
         $this->post($thread->path().'/replies', $reply->toArray());
 
         $this->get($thread->path())
             ->assertSee($reply->body);
     }
+
+    /** @test */
+    function a_reply_requires_a_body()
+    {
+        $this->withExceptionHandling()->signIn();
+
+        $thread = create('App\Thread');
+        $reply = make('App\Reply', ['body' => null]);
+
+        $this->post($thread->path() . '/replies', $reply->toArray())
+            ->assertSessionHasErrors('body');
+
+    }
+
 }
